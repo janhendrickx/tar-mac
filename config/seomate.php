e06a80e9d9e35267962a27874e0c2326095aa6ab
Jan Hendrickx <jan@janhendrickx.be>
2026-01-21 12:23:58 +0100

    SEO Matic corrections

--- config/seomate.php
+++ config/seomate.php
@@ -1,22 +1,22 @@
 <?php
 
 use craft\helpers\App;
-
 return [
+    'defaultProfile' => 'standard',
     'sitename' => \Craft::$app->getSites()->getCurrentSite()->name,
     'defaultMetaImage' => '/assets/img/logo.png',
     
-    'fieldMapping' => [
-        'title'       => ['seoTitle', 'title'],
-        'description' => ['seoDescription', 'summary'],
-        // We voegen hier de transform instructies toe
-        'image'       => [
-            'seoImage', 
-            'image', 
-            ['transform' => ['width' => 1200, 'height' => 630, 'mode' => 'crop']]
-        ],
+    'fieldProfiles' => [
+        'standard' => [
+            'title'       => ['seoTitle', 'title'],
+            'description' => ['seoDescription'],
+            'image'       => [
+                'seoImage', 
+                'image', 
+                ['transform' => ['width' => 1200, 'height' => 630, 'mode' => 'crop']]
+            ],
+        ]
     ],
-    
     'applyTerminator' => true,
     'separator' => ' | ',
 ];
